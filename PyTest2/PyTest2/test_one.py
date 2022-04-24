44d30ed8692d36934f9592b7d7ad71238b3f7fc6
mucahitdemir <mucahitdemirc@gmail.com>
2022-04-24 16:04:34 +0300

    first commit for test scripts

---
+++ PyTest2/PyTest2/test_one.py
@@ -0,0 +1,8 @@
+'''
+Created on May 23, 2021
+
+@author: Mücahit
+'''
+
+def test_passed():
+    assert (3,4,5) == (3,4,5)
---
+++ PyTest2/PyTest2/test_two.py
@@ -0,0 +1,7 @@
+'''
+Created on May 23, 2021
+
+@author: Mücahit
+'''
+def test_failed():
+    assert (3,4,5) == (5,4,3)
